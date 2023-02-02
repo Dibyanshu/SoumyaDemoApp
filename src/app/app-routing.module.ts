957a4aa68c59f8b2a43cd9ca412edce98ddce42e
Soum-lab <122249411+Soum-lab@users.noreply.github.com>
2023-02-02 11:02:31 +0530

    view and edit employee details
    Getting error while click on submit button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { EditEmployeeComponent } from './edit-employee/edit-employee.component';
+import { ViewEmployeeComponent } from './view-employee/view-employee.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent },
-  { path: 'AddEmployee', component: AddEmployeeComponent }
+  { path: 'AddEmployee', component: AddEmployeeComponent },
+  { path: 'ViewEmployee/:employeeId', component: ViewEmployeeComponent },
+  { path: 'EditEmployee/:employeeId', component: EditEmployeeComponent }
+
 ];
 
 @NgModule({
